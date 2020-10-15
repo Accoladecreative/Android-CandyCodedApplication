eaed04bfe109144f93b330868b4e69f0772886b7
Accoladecreative <68949219+Accoladecreative@users.noreply.github.com>
2020-10-15 04:37:44 +0100

    Update MainActivity.java

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -63,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("AsyncHttpClient", "response = " + response);
                         Gson gson = new GsonBuilder().create();;
                         candies = gson.fromJson(response, Candy[].class);
-
                         addCandiesToDatabase(candies);
 
                         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
@@ -82,6 +82,16 @@ public class MainActivity extends AppCompatActivity {
     // ***
     // TODO - Task 1 - Show Store Information Activity
     // ***
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(this,InfoActivity.class);
+
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
+    }
+
 
     private void addCandiesToDatabase(Candy[] candies) {
         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
